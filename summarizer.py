814288219d1a44c25f297bd47c87cb01df54e27b
Songfeng Wu <sfwu120@gmail.com>
2024-10-11 21:33:20 -0400

    use T5-small model instead

--- summarizer.py
+++ summarizer.py
@@ -1,11 +1,11 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import T5Tokenizer, T5ForConditionalGeneration
 import torch
 
 model_dir="./fine_tuned_model"
 
 def load_fine_tuned_model():
-    tokenizer = GPT2Tokenizer.from_pretrained(model_dir)
-    model = GPT2LMHeadModel.from_pretrained(model_dir)
+    tokenizer = T5Tokenizer.from_pretrained(model_dir)
+    model = T5ForConditionalGeneration.from_pretrained(model_dir)
     return model,tokenizer
 
 def prepare_input_for_generation(article):
@@ -14,45 +14,36 @@ def prepare_input_for_generation(article):
 def extract_summary(result):
     return result.split('</s>')[-1].strip()
 
-def combined_score(summary, score, length_weight=0.5):
-    length = len(summary)
-    return score / (length ** length_weight)
-
 def summarize(model,
               tokenizer,
               article,
-              max_length=250,
-              top_k=5,
-              top_p=0.7):
+              max_length=100,
+              top_k=20,
+              top_p=1):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     input_text = prepare_input_for_generation(article)
-    input_ids = tokenizer.encode(input_text, return_tensors='pt').to(device)
+    input_ids = tokenizer.encode(input_text, max_length=512, padding=True, truncation=True, return_tensors='pt').to(device)
 
-    generated_sequences = model.generate(
+    summary_ids = model.generate(
         input_ids,
-        max_length=max_length,  # Set maximum length for the generated summary
-        top_k=top_k,              # Limit the number of highest probability tokens
-        top_p=top_p,             # Use nucleus sampling
-        num_return_sequences=top_k,
+        max_length=max_length,
+        top_k=top_k,
+        top_p=top_p,
         pad_token_id=tokenizer.pad_token_id,
         do_sample=True,
-        output_scores=True,
-        return_dict_in_generate=True
+        early_stopping=True
     )
-
-    decoded_sequences = [tokenizer.decode(seq, skip_special_tokens=True) for seq in generated_sequences.sequences]
-    generated_summaries = [extract_summary(result) for result in decoded_sequences]
-    scores = generated_sequences.scores
-    average_scores = [torch.mean(torch.softmax(score, dim=-1)).item() for score in scores]
-    scored_summaries = [(summary, combined_score(summary, average_scores[i])) for i, summary in enumerate(generated_summaries)]
-    best_summary = sorted(scored_summaries, key=lambda x: -x[1])[0][0]
-
-    return best_summary
+    all_result = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    generated_summary = extract_summary(all_result)
+    return generated_summary
 
 
 def main():
+    tokenizer = T5Tokenizer.from_pretrained("t5-small")
+    model = T5ForConditionalGeneration.from_pretrained("t5-small")
     input_article = "the bishop of the fargo catholic diocese in north dakota has exposed potentially hundreds of church members in fargo, grand forks and jamestown to the hepatitis a virus in late september and early october. the state health department has issued an advisory of exposure for anyone who attended five churches and took communion. bishop john folda (pictured) of the fargo catholic diocese in north dakota has exposed potentially hundreds of church members in fargo, grand forks and jamestown to the hepatitis a . state immunization program manager molly howell says the risk is low, but officials feel it is important to alert people to the possible exposure. the diocese announced on monday that bishop john folda is taking time off after being diagnosed with hepatitis a. the diocese says he contracted the infection through contaminated food while attending a conference for newly ordained bishops in italy last month. symptoms of hepatitis a include fever, tiredness, loss of appetite, nausea and abdominal discomfort. fargo catholic diocese in north dakota (pictured) is where the bishop is located ."
-    print(summarize(input_article))
+
+    print(summarize(model,tokenizer,input_article))
 
 if __name__ == '__main__':
     main()
--- train.py
+++ train.py
@@ -2,12 +2,9 @@ import numpy as np
 import pandas as pd
 import os
 import re
-from transformers import GPT2Tokenizer, GPT2LMHeadModel, GPT2TokenizerFast, Trainer, TrainingArguments, default_data_collator
+from transformers import T5Tokenizer, T5ForConditionalGeneration, Trainer, TrainingArguments, default_data_collator, AutoTokenizer
 from datasets import Dataset, load_metric
 import torch
-import nltk
-from nltk.corpus import stopwords
-from tqdm import tqdm
 import contractions
 
 # DataSet Source
@@ -31,9 +28,7 @@ def short_text(df):
     df['article_length'] = pd.to_numeric(df['article_length'], errors='coerce')
     df['summary_length'] = pd.to_numeric(df['summary_length'], errors='coerce')
 
-    return df[(df['article_length'] <= 900) & (df['summary_length'] <= 900)]
-
-
+    return df[(df['article_length'] <= 500) & (df['summary_length'] <= 500)]
 
 def normalize_text(text):
     # Remove leading/trailing whitespace
@@ -57,22 +52,14 @@ def expand_contractions_text(text):
     return contractions.fix(text)
 
 def standardize_possessives(text):
-    # Example: Convert "US's" to "US is" if appropriate
-    # Note: This is context-dependent and should be used carefully
+    # Example: Convert "US's" to "US has" if appropriate
     text = re.sub(r"(\b\w+)'s\b", r"\1 is", text)
     return text
 
-def remove_stopwords(text, stop_words):
-    tokens = nltk.word_tokenize(text)
-    tokens = [word for word in tokens if word.lower() not in stop_words]
-    return ' '.join(tokens)
-
 def format_all(df_list):
     for df in df_list:
-
         df.drop_duplicates(subset=['article', 'highlights'], inplace=True)
 
-        # change it's to it is etc
         df['article'] = df['article'].apply(expand_contractions_text)
         df['highlights'] = df['highlights'].apply(expand_contractions_text)
 
@@ -88,71 +75,55 @@ def format_all(df_list):
         df['article'] = df['article'].str.lower()
         df['highlights'] = df['highlights'].str.lower()
 
-def add_special_tokens():
-    """ Returns GPT2 tokenizer after adding separator and padding tokens """
-    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-    special_tokens = {'pad_token':'<|pad|>','sep_token':'<|sep|>'}
-    num_add_toks = tokenizer.add_special_tokens(special_tokens)
-    return tokenizer
 
 def prepare_input_text(article, summary):
     return f"{article} </s> {summary}"
 
-def generate_input_text_for_all(df_list):
-    for df in df_list:
-        df['input_text'] = df.apply(lambda x: prepare_input_text(x['article'], x['highlights']), axis=1)
 
-def tokenize_function(df, tokenizer, ignore_idx):
-    encodings = tokenizer(df['input_text'],
-                          padding='max_length',
-                          truncation=True,
-                          max_length=800
-                          )
-    # Create labels, setting padding tokens to -100 for loss calculation
-    encodings['labels'] = [[-100 if token == ignore_idx else token for token in input_ids] for input_ids in encodings['input_ids']]
-    return encodings
+def tokenize_function(df, tokenizer):
+    inputs = df['article']
+    model_inputs = tokenizer(inputs, max_length=512, padding='max_length', truncation=True)
 
-def compute_metrics(eval_pred, tokenizer):
+    with tokenizer.as_target_tokenizer():
+        labels = tokenizer(df['highlights'], max_length=150, padding='max_length', truncation=True)
+
+    model_inputs['labels'] = labels['input_ids']
+    return model_inputs
+
+def compute_metrics(eval_pred,tokenizer):
     rouge = load_metric("rouge")
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
+    predictions, labels = eval_pred
+    # Decode the predicted token IDs to strings
     decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
-    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    # Decode the label token IDs to strings
     decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
-    batch_size = 10  # Adjust based on memory capacity
-    rouge_results = []
-
-    # Process ROUGE calculation in smaller batches
-    for i in range(0, len(decoded_preds), batch_size):
-        batch_preds = decoded_preds[i:i + batch_size]
-        batch_labels = decoded_labels[i:i + batch_size]
-        result = rouge.compute(predictions=batch_preds, references=batch_labels, use_stemmer=True)
-        rouge_results.append(result)
-
-    # Aggregate results
-    final_result = {"rouge2": {"fmeasure": np.mean([r["rouge2"].mid.fmeasure for r in rouge_results])}}
-    return final_result
+    # Compute ROUGE scores
+    results = rouge.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
+    # Extract ROUGE-2 scores
+    rouge2 = results["rouge2"]
+    return {"rouge2": round(rouge2.fmeasure * 100, 2)}  # Return F1 score as percentage
 
 def build_train_args(learning_rate,
                      per_device_train_batch_size,
                      per_device_eval_batch_size,
                      num_train_epochs,
                      weight_decay,
-                     fp16=True):
-    return TrainingArguments(output_dir='./results',
+                     fp16=True,
+                     metric="loss"):
+    return  TrainingArguments(output_dir='./results',
                                 evaluation_strategy="steps",
-                                eval_steps=1000,
+                                eval_steps=500,
                                 learning_rate=learning_rate,
                                 per_device_train_batch_size=per_device_train_batch_size,
                                 per_device_eval_batch_size=per_device_eval_batch_size,
                                 num_train_epochs=num_train_epochs,
                                 weight_decay=weight_decay,
                                 fp16=fp16,
-                                # load_best_model_at_end=True,
-                                # metric_for_best_model="rouge2",
+                                metric_for_best_model=metric,
                                 # greater_is_better=True,
-                                remove_unused_columns=False
+                                remove_unused_columns=True,
+                                load_best_model_at_end=True
                             )
 
 
@@ -160,10 +131,6 @@ def main():
     input_files = load_input_dataset_files()
     raw_train_all_df = input_files['train']
     raw_validation_all_df = input_files['validation']
-
-    nltk.download('stopwords')
-    nltk.download('punkt_tab')
-    stop_words = set(stopwords.words('english'))
 
     train_all_df = short_text(raw_train_all_df)
     validation_all_df = short_text(raw_validation_all_df)
@@ -176,33 +143,22 @@ def main():
 
     format_all([train_df_copy, validation_df_copy])
 
-    # Load the GPT-2 tokenizer and model
-    # Move the model to GPU if possible
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    model = GPT2LMHeadModel.from_pretrained('gpt2')
-    tokenizer = add_special_tokens()
-    model.resize_token_embeddings(len(tokenizer))
-    ignore_idx = tokenizer.pad_token_id
+    tokenizer = T5Tokenizer.from_pretrained("t5-small")
+    model = T5ForConditionalGeneration.from_pretrained("t5-small")
     model.to(device)
 
-    generate_input_text_for_all([train_df_copy, validation_df_copy])
-
-    train_dataset = Dataset.from_pandas(train_df_copy[['input_text']])
-    validation_dataset = Dataset.from_pandas(validation_df_copy[['input_text']])
+    train_dataset = Dataset.from_pandas(train_df_copy)
+    validation_dataset = Dataset.from_pandas(validation_df_copy)
 
     train_dataset = train_dataset.map(tokenize_function, batched=True)
     validation_dataset = validation_dataset.map(tokenize_function, batched=True)
 
-    train_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
-    validation_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
-
-    # Initialize the Trainer
     trainer = Trainer(
         model=model,
-        args=build_train_args(learning_rate=2e-5,
-                              per_device_train_batch_size=6,
-                              per_device_eval_batch_size=6,
+        args=build_train_args(learning_rate=5e-6,
+                              per_device_train_batch_size=32,
+                              per_device_eval_batch_size=32,
                               num_train_epochs=3,
                               weight_decay=0.01),
         train_dataset=train_dataset,
@@ -211,7 +167,6 @@ def main():
     )
 
     trainer.train()
-
     model.save_pretrained('./fine_tuned_model')
     tokenizer.save_pretrained('./fine_tuned_model')
 
